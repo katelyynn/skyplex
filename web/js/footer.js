cad212fbdb96b09cd4b38e7627c50b8fc5aadaf7
plexion <46572320+plexiondev@users.noreply.github.com>
2022-05-18 21:27:32 +0100

    basic skyplex site

---
+++ web/js/footer.js
@@ -0,0 +1,15 @@
+
+// load footer
+
+
+document.getElementById('footer').innerHTML =
+(`
+<div class="footer-inner">
+<span class="main">
+    <p>plexion 2022.</p>
+</span>
+<span class="links">
+    <a href="/">Home</a>
+</span>
+</div>
+`);
