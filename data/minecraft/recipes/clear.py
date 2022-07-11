031d5e8bab1e66959b1b41682701934ab81bae02
plexion <46572320+plexiondev@users.noreply.github.com>
2022-07-11 18:05:37 +0100

    overwrite recipes

--- data/minecraft/recipes/clear.py
+++ data/minecraft/recipes/clear.py
@@ -4,4 +4,18 @@ import os
 
 for filename in os.scandir():
     if filename.is_file() and filename.path != '.\clear.py':
-        print(filename.path)
+        with open(filename.path,'w') as file:
+            file.write('''{
+    "type": "minecraft:crafting_shaped",
+    "key": {
+        "#": {
+        "item": "minecraft:fire"
+        }
+    },
+    "pattern": [
+        "#"
+    ],
+    "result": {
+        "item": "minecraft:fire"
+    }
+}''')
